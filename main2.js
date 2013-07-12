1e33132122c9479a356c7b9fb979cf6145392a35
ogawakeisuke <ogawa@uniba.jp>
2013-07-12 15:57:03 +0900

    現行

--- main2.js
+++ main2.js
@@ -9,35 +9,19 @@ $(document).ready(function() {
       b2 = 0.15 * coef, 
       k = 0.175;
 
-    this.val = 0;
+    this.val = {};
 
     this.dynamic = function(count_is) {
       var base1 = a1 * Math.sin(b1 * count_is );
       var base2 = a2 * Math.cos(b2 * count_is);
       var dynamics = base1 / base2;
 
-      this.val = k * Math.sin(dynamics) * 0.2;
-    };
+      this.val = { 
+        x_is : k * Math.cos(dynamics) * 0.2,
+        y_is : k * Math.sin(dynamics) * 0.2
+      };
+    }
   }
-
-  
-  /*
-  // とりあえずサイン波オシレータ
-  */
-
-  function sinetone(freq) {
-    var phase = 0,
-        phaseStep = freq / pico.samplerate;
-    return {
-        process: function(L, R) {
-            for (var i = 0; i < L.length; i++) {
-                L[i] = R[i] = Math.sin(6.28318 * phase) * 0.25;
-                phase += phaseStep;
-            }
-        }
-    };
-  }
-
 
 
   /*
@@ -52,50 +36,46 @@ $(document).ready(function() {
   // 実行 リファクタリング必要
   */
   $("#start").on("click", function() { 
+
+    //
+    // setup
+    //
     var count = 0;
-    var num = 128;
+    var num = 10;
     var cycloydArray = new Array(num);
+    var tArray = new Array(num);
     var threshould = 0;
     var circ;
     for(var i = 0; i < cycloydArray.length; ++i) { 
       cycloydArray[i] = new Cycloyd(Math.random() * 10);
+
+      
+      tArray[i] = T("tri", {freq:0, mul:0.02})
+      tArray[i].play();
     };
 
-    var paper = Raphael("svg", 1280, 1000);
+    //
+    // 描画のsetup
+    //
+    var paper = Raphael("svg", 1000, 1000);
 
-
+    //
+    // draw
+    //
     setInterval(function() {
       count = counter(count);
       paper.clear();
       for(var i = 0; i < cycloydArray.length; ++i) { 
         cycloydArray[i].dynamic(count);
         
-        circ = paper.circle(cycloydArray[i].val * 30000 + 400,150, 7);
+        circ = paper.circle(500 + Math.sin(cycloydArray[i].val.x_is) * 3000, 250 + Math.sin(cycloydArray[i].val.y_is) * 3000, 2);
+        tArray[i].freq.value = cycloydArray[i].val.y_is * 30000;
       }
-
-      pico.play(sinetone(count));
-
-      // pico.play({
-      //   process: function(L, R) {
-      //     for (var i = 0; i < L.length; i++) {
-      //       L[i] = R[i] = threshould;
-      //     }
-      //   }
-      // });
+      //console.log(Math.cos(cycloydArray[0].val.x_is * 0.5) + 3000);
+      
 
     },20);
 
-  })
-
-  $(window).mousemove(function(e){
-    var dx = e.clientX;
-    
-    pico.play(sinetone(dx));
-  });
-
-
-  $("#stop").on("click", function() { 
-    pico.pause();
   })
 
 });
