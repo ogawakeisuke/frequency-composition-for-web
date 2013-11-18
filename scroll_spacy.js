6b17383aebc9086649012f1fdc84a3fc36b73419
ogawakeisuke <ogawa@uniba.jp>
2013-11-18 17:57:16 +0900

    むずかしい

--- scroll_spacy.js
+++ scroll_spacy.js
@@ -8,18 +8,26 @@ $(document).ready(function() {
     // グローバルな変数
     var num = 50;
     var tArray = new Array(num);
-    var buffer = new Float32Array(44100);
-
+    var src = wavb("36454d4b41362f303639332309efd9cc362f220df2d9c8c3c6cbccc6bab0aeb7");  
 
     //
     // setup関数
     //
     function timbleInit() {
-      var src = "wavb(36454d4b41362f303639332309efd9cc362f220df2d9c8c3c6cbccc6bab0aeb7)";
+
 
       for(var i = 0; i < num; ++i) { 
-        tArray[i] = T("osc", { freq: Math.random()*200 + 200, mul:0.03, wave:buffer, wave:src }).play();
+        tArray[i] = T("osc", { freq: Math.random()*200 + 200, mul:0.03, wave:src }).play();
       };
+    }
+    function wavb(val) {
+      return "wavb("+val+")"
+    }
+
+    function aTowevb() {
+      return $.map( tArray, function(val) {
+        return parseInt(val.freq.value).toString(16);
+      }).join("");
     }
 
 
@@ -29,20 +37,19 @@ $(document).ready(function() {
 
 
   $(window).scroll(function () {
-
+    src = wavb(aTowevb()); 
+    
     for(var i = 0; i < num; ++i) { 
-      var table = [0.5, [0, 500]];
+      
+      tArray[i].wave = src;
       if (tArray[i].freq.value < tArray[i].freq.value + $(window).scrollTop() * 0.01 ){
         tArray[i].freq.value += $(window).scrollTop() * 0.01;
-      
       }else{
         tArray[i].freq.value -= $(window).scrollTop() * 0.01;
       }
-      // T("env", {table:table}, tArray[i]).on("ended", function() {
-      //   this.pause();
-      // }).bang().play();
+      
     }
-    console.log($(window).scrollTop() );
+    
   });
 
 
