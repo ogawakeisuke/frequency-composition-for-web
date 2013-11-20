283c3ea58c7087cc4bb03316ae8d6cd1b37bacaf
ogawakeisuke <ogawa@uniba.jp>
2013-11-20 18:14:43 +0900

    インターバルのばらつかせ方が分かってきた

--- scroll_spacy.js
+++ scroll_spacy.js
@@ -6,8 +6,11 @@ $(document).ready(function() {
   (function() { 
 
     // グローバルな変数
-    var num = 50;
-    var tArray = new Array(num);
+    var num = 10;
+    var oscArray = new Array(num);
+    var oscArrayOriginFreq = new Array(num);
+    var intervalArray = new Array(num);
+    var intervalArrayOriginTime = new Array(num);
     var src = wavb("00112233445566778877665544332211");  
 
 
@@ -26,18 +29,26 @@ $(document).ready(function() {
     // setup関数
     //
     function timbleInit() {
-
-
       for(var i = 0; i < num; ++i) { 
-        tArray[i] = T("osc", { freq: Math.random()*200 + 200, mul:0.03, wave:src }).plot({target:wavbase}).play();
-      };
+        
+        oscArray[i] = T("tri", { freq: Math.random()*300 + 400, mul:0.03, env:env}).plot({target:wavbase});
+        oscArrayOriginFreq[i] = oscArray[i].freq.value;
+
+        var env = T("perc", {a:10, r: 30 * (Math.random()*30)}, oscArray[i]).bang().play();
+
+        var interval = T("param", {value: 20 })
+        intervalArrayOriginTime[i] = interval.value
+        intervalArray[i] = T("interval", {interval:interval}, env).start();
+
+      }
     }
+
     function wavb(val) {
       return "wavb("+val+")"
     }
 
     function aTowevb() {
-      return $.map( tArray, function(val) {
+      return $.map( oscArray, function(val) {
         return parseInt(val.freq.value).toString(16);
       }).join("");
     }
@@ -49,18 +60,13 @@ $(document).ready(function() {
 
 
   $(window).scroll(function () {
-    //src = wavb(aTowevb()); 
-    
+    var sin_val = Math.sin($(window).scrollTop() * 0.001);
+    //console.log(intervalArray[0].interval.value = sin_val * 100 );
     for(var i = 0; i < num; ++i) { 
-      
-      tArray[i].wave = src;
-      if (tArray[i].freq.value < tArray[i].freq.value + $(window).scrollTop() * 0.01 ){
-        tArray[i].freq.value += $(window).scrollTop() * 0.01;
-      }else{
-        tArray[i].freq.value -= $(window).scrollTop() * 0.01;
-      }
-      
+      oscArray[i].freq.value = 200 + sin_val * oscArrayOriginFreq[i];
+      intervalArray[i].interval.value = Math.sqrt( (sin_val * ( Math.random() * 400 ) * intervalArrayOriginTime[i] ) * (sin_val * ( Math.random() * 400 ) * intervalArrayOriginTime[i] ) )
     }
+    console.log(intervalArray[0].interval.value);
     
   });
 
